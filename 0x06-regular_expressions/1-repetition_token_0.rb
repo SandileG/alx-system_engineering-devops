121fed123eff4b52c1609186fc2f2a16485d2835
SandileG <sandilegraham@gmail.com>
2024-02-04 05:22:20 -0800

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -7,17 +7,17 @@ if ARGV.length != 1
 end
 
 # The regular expression to match "School"
-regexp = /hbttn/
+regexp = /hbt{2,5}n/
 
 # Extracting the argument
 input = ARGV[0]
 
 # Using the match method to find the first occurence of the pattern
-match_result = input.match(regexp)
+matches = input.scan(regexp)
 
 # Check if there is a match and print the result
-if match_result
-  puts match_result[0]
-else
+if matches.empty?
   puts ""
+else
+  puts matches.join
 end
