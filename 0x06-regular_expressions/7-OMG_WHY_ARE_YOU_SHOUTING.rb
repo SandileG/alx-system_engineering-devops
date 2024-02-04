c65efddc4a7b3bc5e808f223850c38975132bab3
SandileG <sandilegraham@gmail.com>
2024-02-04 11:16:35 -0800

    7-OMG_WHY_ARE_YOU_SHOUTING.rb

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Ensure an argument is provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# The regular expression to match only capital letters
+regexp = /[A-Z]/
+
+# Extracting the argument
+input = ARGV[0]
+
+# Check if there are matches and print the result
+if matches.empty?
+  puts ""
+else
+  puts matches.join
+end
