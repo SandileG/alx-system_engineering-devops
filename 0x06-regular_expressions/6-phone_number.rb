0bd6baba09ca68e53969f7778bb04fbd3d43d229
SandileG <sandilegraham@gmail.com>
2024-02-04 09:16:48 -0800

    4-repetition_token_3.rb

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Ensure an argument is provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME <phone_number>}"
+  exit 1
+end
+
+# The regular expression to match a 10-digit phone number
+regexp = /^\d{10}$/
+
+# Extracting the argument
+phone_number = ARGV[0]
+
+# Check if the input matches the pattern
+if phone_number.match?(regexp)
+  puts "#{phone_number}$"
+else
+  puts ""
+end
