72149e82c2aa0e6b5a20078e5b9043adf4115b7b
SandileG <sandilegraham@gmail.com>
2024-02-04 08:53:52 -0800

    4-repetition_token_3.rb

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Ensure an argument is provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# The regular expression to match "hbt*n" where 't' can occur 0 or more times
+regexp = /hbt*t/
+
+# Extracting the argument
+input = ARGV[0]
+
+# Using the scan method to find all occurrences of the pattern
+matches = input.scan(regexp)
+
+# Check if there are matches and print the result
+if matches.empty?
+  puts ""
+else
+  puts matches.join
+end
