7b2068e4dbd1dcbf17108f472301c4df0625275c
SandileG <sandilegraham@gmail.com>
2024-03-25 07:38:52 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,6 @@
-# Define a class for clarity
-class { 'flask':
-  # Ensure flask is installed with pip3
-  package { 'flask':
-    ensure => installed,
-    provider => 'pip3',
-    # Specify the exact version
-    require => Class['python::pip3'],  # Make sure pip3 is installed first
-  }
-
-  # Specify the exact version (optional, for clarity)
-  $flask_version = '2.1.0'
-  # Enforce version using pip options (recommended)
-  package { 'flask':
-    ensure => $flask_version,
-    provider => 'pip3',
-    require => Class['python::pip3'],
-  }
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
