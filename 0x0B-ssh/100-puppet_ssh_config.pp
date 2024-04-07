db0e0f819a8a0c5f4b52b9330b2f0155c9bf3850
SandileG <sandilegraham@gmail.com>
2024-04-06 17:47:21 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Make config changes using Puppet
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
