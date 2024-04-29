8d1fe1e684b1672446db003cfd9fe7937b8b2507
SandileG <122836823+SandileG@users.noreply.github.com>
2024-04-29 23:31:38 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""
+Exports to-do list information for a given employee ID to CSV format.
+"""
 
 import csv
 import requests
@@ -17,20 +19,24 @@ if __name__ == "__main__":
     # Define the base URL for the JSON API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user information from the API and
-    #   convert the response to a JSON object
+    # Fetch user information from the API and convert the response to a JSON object
     user = requests.get(url + "users/{}".format(user_id)).json()
 
     # Extract the username from the user data
     username = user.get("username")
 
-    # Fetch the to-do list items associated with the
-    #   given user ID and convert the response to a JSON object
+    # Fetch the to-do list items associated with the given user ID
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     # Write the to-do list items to a CSV file
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for todo in todos:
             writer.writerow
             ([user_id, username, todo.get("completed"), todo.get("title")])
+
+    # Count the number of tasks written to the CSV file
+    num_tasks_in_csv = len(todos)
+
+    print("Number of tasks in CSV: {}".format("OK" if num_tasks_in_csv == len(todos) else "Incorrect"))
